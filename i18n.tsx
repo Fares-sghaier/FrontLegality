b6b86d569896fc9a777b5b773c1ef6b64ab535d3
aziztheiri <alatheiri123@gmail.com>
2024-07-05 15:48:52 +0100

    ..

--- i18n.tsx
+++ i18n.tsx
@@ -1,5 +1,3 @@
-// /src/i18n.js
-
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
@@ -8,13 +6,17 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 const Languages = ['en', 'fr'];
 
 i18n
-  .use(Backend) // load translations using http
+  .use(Backend) // load translations using http backend
   .use(LanguageDetector) // detect user language
   .use(initReactI18next) // pass the i18n instance to react-i18next
   .init({
     fallbackLng: 'en', // default language
     debug: true, // enable debug mode
     supportedLngs: Languages, // supported languages
+
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json', // path to translations
+    },
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
